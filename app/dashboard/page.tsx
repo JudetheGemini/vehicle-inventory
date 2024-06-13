9eac9db55906717a103081b5401e2078d5264071
Jude Okorie <jude58005@gmail.com>
2024-06-13 12:54:12 +0100

    added recent additions on dashboard homepage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,16 @@ import { useEffect, useState } from "react";
 import { type Vehicle } from "@/src/API";
 import { listVehicles } from "@/src/graphql/queries";
 
-import { Grid, Card, Text, Divider, Flex, Button, Modal } from "@mantine/core";
+import {
+  Grid,
+  Card,
+  Text,
+  Divider,
+  Flex,
+  Button,
+  Modal,
+  Table,
+} from "@mantine/core";
 
 const client = generateClient();
 
@@ -18,6 +27,11 @@ export default function Dashboard() {
   const [vehicles, setVehicles] = useState<Vehicle[]>([]);
   const { setTotalVehicles, totalVehicles } = useVehicleStore((state) => state);
   const testStoreData = useLoginStore((state) => state.test);
+  const sortedCars = [...vehicles].sort((a, b) => {
+    return Date.parse(b.createdAt) - Date.parse(a.createdAt);
+  });
+
+  const slicedSortedCars = sortedCars.slice(0, 5);
 
   const handleLogout = () => {
     signOut();
@@ -92,6 +106,59 @@ export default function Dashboard() {
       <Text size="lg" fw={700}>
         Welcome to your dashboard {user?.username}
       </Text>
+      <Grid grow>
+        <Grid.Col span={6}>
+          <Card withBorder>
+            <Text fw={700} fs="12">
+              Total Vehicles
+            </Text>
+            <Divider my="md" />
+            <Text fw={700} fs="24">
+              {totalVehicles}
+            </Text>
+          </Card>
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Card withBorder>
+            <Text fw={700} fs="12">
+              Recent Additions
+            </Text>
+            <Divider my="md" />
+            {/* <Text fw={700} fs="24">
+              <ul>
+                {sortedCars.map((vehicle) => (
+                  <li className="list-disc" key={vehicle.id}>
+                    <Text fw={500}>
+                      {vehicle.make} {vehicle.model} -{" "}
+                      {new Date(vehicle.createdAt).toDateString()}
+                    </Text>
+                  </li>
+                ))}
+              </ul>
+            </Text> */}
+            <Table verticalSpacing="md">
+              <Table.Thead>
+                <Table.Tr>
+                  <Table.Th>Car</Table.Th>
+                  <Table.Th>Added On</Table.Th>
+                </Table.Tr>
+              </Table.Thead>
+              <Table.Tbody>
+                {slicedSortedCars.map((vehicle) => (
+                  <Table.Tr key={vehicle.id}>
+                    <Table.Td>
+                      {vehicle.make} {vehicle.model}
+                    </Table.Td>
+                    <Table.Td>
+                      {new Date(vehicle.createdAt).toDateString()}
+                    </Table.Td>
+                  </Table.Tr>
+                ))}
+              </Table.Tbody>
+            </Table>
+          </Card>
+        </Grid.Col>
+      </Grid>
       <div className="absolute bottom-10 right-10 w-full flex justify-end items-center"></div>
     </div>
   );
